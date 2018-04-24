4d21c94863d0f3215d4b2aca3b5e4a57464dea9a
HipsterBeard <uddtru16@student.westerdals.no>
2018-04-24 12:54:48 +0200

    push

--- Mappeinnlevering 2/EatInOsloMVC/Controllers/UserRestaurantController.cs
+++ Mappeinnlevering 2/EatInOsloMVC/Controllers/UserRestaurantController.cs
@@ -1,9 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
+using EatInOsloMVC.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace EatInOsloMVC.Controllers{
     public class UserRestaurantController:Controller{
+
+        private readonly EatInOsloContext _context;
+
+        public UserRestaurantController(EatInOsloContext context){
+            _context = context;
+        }
         public IActionResult Welcome(){
             return View();
+        }
+
+        public async Task<IActionResult> ShowRestaurants(){
+            List<Restaurant> restaurantList = await _context.Restaurant.ToListAsync();
+            return View(restaurantList);
         }
 
         public IActionResult ShowReviews(){
